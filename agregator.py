736a138498410457aeed55dc114380be98638011
Igor Kuzmenko <kuzmenko.ig@gmail.com>
2016-09-03 17:24:21 +0300

    add requirement file with requests

--- agregator.py
+++ agregator.py
@@ -1,10 +1,8 @@
 import requests
 
-
 def get_content(url):
     """
     Get web page in bytes format by url. If something wrong return None
-
     >>> type(get_content(url='http://itea.ua/courses-itea/python/python-advanced/'))
     <class 'bytes'>
     >>> type(get_content('hts://pysad2asdmotw.com/3/'))
@@ -13,6 +11,11 @@ def get_content(url):
 
     try:
         if requests.head(url).status_code == 200:
+            page = requests.get(url)
+            if page.encoding:
+                contet = page.content.decode(page.encoding)
             return requests.get(url).content
     except requests.exceptions.RequestException:
         return None
+
+
