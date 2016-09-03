281697add298536a1c697f9f4032e4297fd28cb4
Igor Kuzmenko <kuzmenko.ig@gmail.com>
2016-09-03 14:25:59 +0300

    my solve

---
+++ agregator.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def get_content(url):
+    """
+    Get web page in bytes format by url. If something wrong return None
+
+    >>> type(get_content(url='http://itea.ua/courses-itea/python/python-advanced/'))
+    <class 'bytes'>
+    >>> type(get_content('hts://pysad2asdmotw.com/3/'))
+    <class 'NoneType'>
+    """
+
+    try:
+        if requests.head(url).status_code == 200:
+            return requests.get(url).content
+    except requests.exceptions.RequestException:
+        return None
